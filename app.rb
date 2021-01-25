1e10068c273fd166d353e9eb0e808e7139730052
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-25 09:59:01 -0600

    commit tree 9d015617138b0ef459d6abc216c780678efd9150

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    
+    erb :show
+  end
 end
